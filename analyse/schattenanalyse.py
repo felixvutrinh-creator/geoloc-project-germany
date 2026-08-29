7ca7bf10960c81330be9b3036c012cb566bb0da6
FeunLx <felixvutrinh@gmail.com>
2026-08-29 18:38:23 +0200

    Adaptiven Threshold eingebaut

--- analyse/schattenanalyse.py
+++ analyse/schattenanalyse.py
@@ -7,7 +7,7 @@ import cv2 as cv
 
 def shadow_suggestion_mask(image_path: str, output_path: str):
     image = cv.imread(image_path)
-    gray = cv.cvtColor(image, cv.ColorConversionCodes.BGR2GRAY)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     blurred = cv.GaussianBlur(gray, (5, 5), 0)
     _, mask = (cv.threshold(blurred, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU))
     contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -15,7 +15,11 @@ def shadow_suggestion_mask(image_path: str, output_path: str):
         if cv.contourArea(contour) > 100:
             cv.drawContours(image, [contour], -1, (0, 0, 255), 2)
     cv.imwrite(output_path, image)
-    return mask
+    # Adaptiv thresholding
+    adaptive_threshold = cv.adaptiveThreshold(blurred, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 101, 2)
+    cv.imwrite(f"{output_path}_adaptive_threshold.png", adaptive_threshold)
+    return adaptive_threshold
+
 if __name__ == "__main__":
     parser = ap.ArgumentParser()
     parser.add_argument("--input_path", type=str, required=True)
@@ -23,4 +27,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
     shadow_suggestion_mask(args.input_path, args.output_path)
     print(f"Shadow suggestion mask saved to {args.output_path}")
-
